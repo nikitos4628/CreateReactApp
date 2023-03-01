75219f2888b310e439f89bc6c295990feac75dfd
nikitos4628 <soba.nikita@mail.ru>
2023-03-02 06:22:34 +0700

    StrictMode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {Component, StrictMode} from 'react';
 import './App.css';
 
 const Header = () => {
@@ -42,7 +42,9 @@ function Btn() {
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <StrictMode>
+			<Header/>
+		</StrictMode>
 		<Field/>
 		<Btn/>
     </div>
