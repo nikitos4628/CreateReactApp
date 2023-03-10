d10ca1d74706082d502e9ee867d394aef5a2a51e
nikitos4628 <soba.nikita@mail.ru>
2023-03-11 06:43:59 +0700

    React.createRef, myRef, ref

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import {Container} from 'react-bootstrap';
 import './App.css';
 
 class Form extends Component {
-	constructor(props) {
-		 super(props);
+	// myRef = React.createRef();
+
+	// componentDidMount() {
+	// 	this.myRef.current.focus();
+	// }
+
+	setInputRef = (elem) => {
+		this.myRef = elem;
+	}
+
+	focusFirstTI = () => {
+		if (this.myRef) {
+			this.myRef.focus();
+		}
 	}
 
 	render() {
@@ -13,15 +25,38 @@ class Form extends Component {
 					<form className="w-50 border mt-5 p-3 m-auto">
 						 <div className="mb-3">
 							  <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-							  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+							  <input
+							  	ref={this.setInputRef} 
+								type="email" 
+								className="form-control" 
+								id="exampleFormControlInput1" 
+								placeholder="name@example.com"/>
 						 </div>
 						 <div className="mb-3">
 							  <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-							  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+							  <textarea onClick={this.focusFirstTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 						 </div>
 					</form>
 			  </Container>
 		 )
+	}
+}
+
+class TextInput extends Component {
+
+	doSmth = () => {
+		console.log('object');
+	}
+
+	render () {
+		return (
+			<input
+				ref={this.myRef} 
+				type="email" 
+				className="form-control" 
+				id="exampleFormControlInput1" 
+				placeholder="name@example.com"/>
+		)
 	}
 }
 
